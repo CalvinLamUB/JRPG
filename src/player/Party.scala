14dc6c004de393ed89f296fa731f493753336320
calvin <calvinla@buffalo.edu>
2019-10-15 02:59:02 -0400

    gui

--- src/player/Party.scala
+++ src/player/Party.scala
@@ -1,8 +1,26 @@
 package player
 
-class Party(party: List[Character]) {
-  def groupFight(party1: Party, party2: Party): Unit ={
-
+class Party(list: List[Character]) {
+  val party = list
+  def partyAlive(): Boolean = {
+    var alive: Boolean = false
+    for(character <- party){
+      if(character.isAlive()){
+        alive = true
+      }
+    }
+    alive
   }
-
+  def partyFight(party2: Party): Unit = {
+    if (!this.partyAlive()) {
+      for (character <- party2.party) {
+        character.levelUp((this.party.length*50)/party2.party.length)
+      }
+    }
+    if (!party2.partyAlive()) {
+      for (character <- this.party) {
+        character.levelUp((party2.party.length*50)/this.party.length)
+      }
+    }
+  }
 }
